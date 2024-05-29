a50e787db46241a3dc0b75b688d283fe9c19a73d
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-05-30 00:16:01 +0200

    Feat: Do the analyzeArray function and tests

--- __tests__/analyzeArray.test.js
+++ __tests__/analyzeArray.test.js
@@ -1,7 +1,26 @@
 import analyzeArray from "../src/analyzeArray";
 
+const object = {
+  average: 4,
+  min: 1,
+  max: 8,
+  length: 6,
+};
+
 describe("\nanalyzeArray: function that takes an array of numbers and returns an object with the following properties: average, min, max, and length", () => {
   test("should exist", () => {
     expect(analyzeArray).toBeDefined();
+  });
+
+  test("analyzeArray(10,'') should return an error", () => {
+    expect(() => analyzeArray(10, "")).toThrow(
+      "The parameter must be an array of numbers!"
+    );
+  });
+
+  test(`analyzeArray([1,8,3,4,2,6]) should return ${JSON.stringify(
+    object
+  )} `, () => {
+    expect(object).toEqual(analyzeArray([1, 8, 3, 4, 2, 6]));
   });
 });
--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,3 +1,27 @@
-const analyzeArray = () => {};
+const analyzeArray = (arr) => {
+  let average, min, max, length;
+  const checkParameter = (el) => {
+    return typeof el === "number";
+  };
+
+  if (Array.isArray(arr) && arr.every((el) => checkParameter(el))) {
+    average = (() => {
+      return arr.reduce((prev, curr) => prev + curr) / arr.length;
+    })();
+    min = (() => {
+      return Math.min(...arr);
+    })();
+    max = (() => {
+      return Math.max(...arr);
+    })();
+    length = (() => {
+      return arr.length;
+    })();
+  } else {
+    throw new Error("The parameter must be an array of numbers!");
+  }
+
+  return { average, min, max, length };
+};
 
 export default analyzeArray;
